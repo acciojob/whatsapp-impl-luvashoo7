4e953a10cfdc1de29bff9c7bcbabead9bca015b0
AshLuvCode <ashishrajstr@gmail.com>
2023-01-28 16:41:05 +0530

    first commit

--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -1,29 +1,41 @@
 package com.driver;
+import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.Date;
+import java.util.List;
 
-import org.springframework.stereotype.Repository;
+@Service
+public class WhatsappService {
+    WhatsappRepository whatsappRepository = new WhatsappRepository();
 
-@Repository
-public class WhatsappRepository {
+    public String createUser(String name, String mobile) throws Exception {
+        return whatsappRepository.createUser(name, mobile);
+    }
 
-    //Assume that each user belongs to at most one group
-    //You can use the below mentioned hashmaps or delete these and create your own.
-    private HashMap<Group, List<User>> groupUserMap;
-    private HashMap<Group, List<Message>> groupMessageMap;
-    private HashMap<Message, User> senderMap;
-    private HashMap<Group, User> adminMap;
-    private HashSet<String> userMobile;
-    private int customGroupCount;
-    private int messageId;
+    public Group createGroup(List<User> users){
+        return whatsappRepository.createGroup(users);
+    }
 
-    public WhatsappRepository(){
-        this.groupMessageMap = new HashMap<Group, List<Message>>();
-        this.groupUserMap = new HashMap<Group, List<User>>();
-        this.senderMap = new HashMap<Message, User>();
-        this.adminMap = new HashMap<Group, User>();
-        this.userMobile = new HashSet<>();
-        this.customGroupCount = 0;
-        this.messageId = 0;
+    public int createMessage(String content){
+        return whatsappRepository.createMessage(content);
+    }
+
+    public int sendMessage(Message message, User sender, Group group) throws Exception{
+
+        return  whatsappRepository.sendMessage(message, sender, group);
+    }
+
+    public String changeAdmin(User approver, User user, Group group) throws Exception{
+        return whatsappRepository.changeAdmin(approver, user, group);
+    }
+
+    public int removeUser(User user) throws Exception{
+
+        return whatsappRepository.removeUser(user);
+    }
+
+    public String findMessage(Date start, Date end, int K) throws Exception{
+
+        return whatsappRepository.findMessage(start, end, K);
     }
 }
